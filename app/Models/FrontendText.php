65074788408072b4a81a303ad4e313823414d840
Suhendra <sayasuhendra@gmail.com>
2026-04-20 17:22:43 +0700

    add public html to ignore.

--- app/Models/FrontendText.php
+++ app/Models/FrontendText.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class FrontendText extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $casts = [
+        'marquee' => 'array',
+        'portfolio' => 'array',
+        'services' => 'array',
+        'showcase' => 'array',
+        'testimonials' => 'array',
+        'cta' => 'array',
+        'gallery' => 'array',
+        'other_categories' => 'array',
+        'footer' => 'array',
+    ];
 }
